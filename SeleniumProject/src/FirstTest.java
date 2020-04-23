6b611199c7fe8d15ff2ff70ddb6dde68453cb694
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-04-22 23:35:20 -0400

    passing yard comparison done

--- SeleniumProject/src/FirstTest.java
+++ SeleniumProject/src/FirstTest.java
@@ -27,12 +27,25 @@ public class FirstTest {
 		int p1Rushing = Integer.parseInt(p1.rushYards);
 		int p2Rushing = Integer.parseInt(p2.rushYards);
 		
+		int p1Passing = Integer.parseInt(p1.passingYards);
+		int p2Passing = Integer.parseInt(p2.passingYards);
+		
 		
 		if(p1Rushing > p2Rushing) {
 			System.out.println(p1.player + " " + "has more total rushing yards");
 		} if(p2Rushing > p1Rushing) {
 			System.out.println(p2.player + " " + "has more total rushing yards");
 		} if(p1Rushing == p2Rushing) {
+			System.out.println("the two players are tied");
+		}
+
+		
+
+		if(p1Passing > p2Passing) {
+			System.out.println(p1.player + " " + "has more total passing yards");
+		} if(p2Passing > p1Passing) {
+			System.out.println(p2.player + " " + "has more total passing yards");
+		} if(p1Passing == p2Passing) {
 			System.out.println("the two players are tied");
 		}
 
